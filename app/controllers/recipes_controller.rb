1299ac1dce5bc14e73624c8011ffea9b8636ceda
Austin Vern Songer <austinvernsonger@outlook.com>
2016-09-26 13:43:46 -0500

    3

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,8 @@
+# Austin Vern Songer
+
+class RecipesController < ApplicationController
+  def index
+    @search_term = params[:search] || 'chocolate'
+    @recipes = Recipe.for(@search_term)
+  end
+end
